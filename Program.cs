2f6dd9338594840831b96ff1ab7d0a412e286e63
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-14 23:36:35 +0900

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//stp_map_task
+
+
+string secondLine = File.ReadLines("C:\\task_1.txt").ElementAtOrDefault(2);
+
+//foreach (string str in secondLine)
+//{
+//    Console.WriteLine(str);
+//}
+
+Console.WriteLine(secondLine);
