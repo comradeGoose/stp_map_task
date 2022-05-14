b85dc94ac78b37cdbefed515c811e204c2075215
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-15 00:35:27 +0900

    вывожу конкретную строку файла в консоль

--- Program.cs
+++ Program.cs
@@ -1,9 +1,26 @@
 ﻿//stp_map_task
-//третья строка 
-string secondLine = File.ReadLines("C:\\task_1.txt").ElementAtOrDefault(2);
-Console.WriteLine(secondLine);
-
 
 //количество строк файла
-int amount_file_lines = File.ReadLines("C:\\task_1.log").Count();
-Console.WriteLine(amount_file_lines);
+int amount_file_lines = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").Count();
+Console.WriteLine("Колличество строк в файле: " + amount_file_lines);
+
+Console.WriteLine();
+
+Console.Write("Введите номер строки которую хотите вывести в консоль: ");
+string input = String.Empty;
+input = Console.ReadLine();
+int num = Int32.Parse(input);
+Console.WriteLine();
+
+for (int i = 0; i < amount_file_lines; i++)
+{
+    if(i == num)
+    {
+        string secondLine = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").ElementAtOrDefault(i - 1);
+        Console.WriteLine(secondLine);
+        return;
+    }
+}
+
+
+
