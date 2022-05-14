6728b51fcc8ac7b95a5f0eb4e7495769b7751bb7
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-15 01:02:47 +0900

    вынек класс (Test_txt_line) в отдельный файл

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿//stp_map_task
-using System.Text;
 
 //количество строк файла
 int amount_file_lines = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").Count();
@@ -19,8 +18,9 @@ for (int i = 0; i < amount_file_lines; i++)
     {
         string secondLine = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").ElementAtOrDefault(i - 1);
         Console.WriteLine(secondLine);
+        Console.WriteLine();
         string newLine = "";
-        foreach(var line in secondLine)
+        foreach(char line in secondLine)
         {
             if(line == ' ')
             {
@@ -34,35 +34,5 @@ for (int i = 0; i < amount_file_lines; i++)
     }
 }
 
-class Test_txt_line
-{
-    public static void Line(string line)
-    {
-        string path = @"c:\Users\User\OneDrive\Desktop\test.txt";
 
-        try
-        {
-            // Создаю файл или перезапишсываю, если файл существует.
-            using (FileStream fs = File.Create(path))
-            {
-                byte[] info = new UTF8Encoding(true).GetBytes(line);
-                // записываю secondLine в файл.
-                fs.Write(info, 0, info.Length);
-            }
-
-            using (StreamReader sr = File.OpenText(path))
-            {
-                string s = "";
-                while ((s = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(s);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-        }
-    }
-}
 
---
+++ Test_txt_line.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+public class Test_txt_line
+{
+    public static void Line(string line)
+    {
+        string path = @"c:\Users\User\OneDrive\Desktop\test.txt";
+
+        try
+        {
+            // Создаю файл или перезапишсываю, если файл существует.
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(line);
+                // записываю secondLine в файл.
+                fs.Write(info, 0, info.Length);
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
