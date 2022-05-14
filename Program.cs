ee271761ded0b2185d4e4d938b2de208d8019ad0
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-15 00:41:04 +0900

    создал класс (Test_txt_line) для вывода в консоль и записи в файл (файл создется если его нет) выбранной строки из файла

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿//stp_map_task
+using System.Text;
 
 //количество строк файла
 int amount_file_lines = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").Count();
@@ -18,9 +19,41 @@ for (int i = 0; i < amount_file_lines; i++)
     {
         string secondLine = File.ReadLines(@"c:\Users\User\OneDrive\Desktop\task_1.log").ElementAtOrDefault(i - 1);
         Console.WriteLine(secondLine);
+        Console.WriteLine();
+        Test_txt_line.Line(secondLine);
         return;
     }
 }
 
+class Test_txt_line
+{
+    public static void Line(string line)
+    {
+        string path = @"c:\Users\User\OneDrive\Desktop\test.txt";
 
+        try
+        {
+            // Создаю файл или перезапишсываю, если файл существует.
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(line);
+                // записываю secondLine в файл.
+                fs.Write(info, 0, info.Length);
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
 
